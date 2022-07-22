f9730b21d7b0c1a43d090258bbf157fd3074604a
Jaskaranvir Deogan <84167202+jaskarandeogan@users.noreply.github.com>
2022-07-22 10:43:26 -0700

    Route Fix: cannot get path

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 
-const server = app.listen(6168, () => console.log("listening"));
+const server = app.listen(6168, () => console.log("listening : 6168"));
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('*', (req, res) => {
-    res.sendFile()
-})
+app.get("/*", function (req, res) {
+  let path = require("path");
+  res.sendFile(path.join(__dirname, "public/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,31 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-devtool:"source-map",
-mode:"development",
-  entry: './src/index.js',
+  devtool: "source-map",
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+   
   },
   module: {
     rules: [
-        {
-          test:/\.css$/,
-          use:['style-loader', 'css-loader']
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
         },
-        { test: /\.m?js$/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react' ]
-            }
-          }
-      }
-    ]
-}
+      },
+    ],
+  },
+  
 };
 
 // npm install webpack webpack-cli style-loader css-loader babel-loader @babel/core @babel/preset-env @babel/preset-react
